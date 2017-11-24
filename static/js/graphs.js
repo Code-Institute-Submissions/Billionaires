88e63b025c0f23a1830cd1f6d143b00dfb450f8a
Conor Clarke <conorc470@gmail.com>
2017-11-24 14:58:09 +0000

    dimple

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -107,3 +107,14 @@ function makeGraphs(error, billionairesData) {
 
    dc.renderAll();
 }
+
+var svg = dimple.newSvg(".dimple", 800, 600);
+    var data = [
+      { "Word":"Hello", "Awesomeness":2000 },
+      { "Word":"World", "Awesomeness":3000 }
+    ];
+    var chart = new dimple.chart(svg, data);
+    chart.addCategoryAxis("x", "Word");
+    chart.addMeasureAxis("y", "Awesomeness");
+    chart.addSeries(null, dimple.plot.bar);
+    chart.draw()
